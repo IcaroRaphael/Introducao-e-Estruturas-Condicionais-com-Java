e37e65184d51443fb651b03ee5f352d2520e8801
IcaroRaphael <icaro2551@gmail.com>
2023-09-01 09:34:30 -0300

    feat: Condicionais Simples

---
+++ Introducao-e-Estruturas-Condicionais-com-Java/src/estruturas_condicionais/condicionais_simples/Main.java
@@ -0,0 +1,15 @@
+package estruturas_condicionais.condicionais_simples;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
+
+        if(valorSolicitado < saldo)
+            saldo = saldo - valorSolicitado;
+
+        System.out.println(saldo);
+
+    }
+}
