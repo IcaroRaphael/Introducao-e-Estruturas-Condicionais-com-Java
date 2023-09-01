74242ea94c49ebcc518e1c2378d6dd7029dc8361
IcaroRaphael <icaro2551@gmail.com>
2023-09-01 09:46:27 -0300

    feat: Condição ternária

---
+++ Introducao-e-Estruturas-Condicionais-com-Java/src/estruturas_condicionais/condicao_ternaria/ResultadoEscolar.java
@@ -0,0 +1,15 @@
+package estruturas_condicionais.condicao_ternaria;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 6;
+
+        // Cenário 1
+        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+
+        // Cenário 2
+        resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
